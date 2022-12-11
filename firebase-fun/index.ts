5390d2201fb6ffa8df6e0130b5a9e34501f8f481
Imeguras <imeguras@teknik.io>
2022-12-11 21:48:30 +0000

    FAS-31 Fixed notification duplicates

--- firebase-fun/index.ts
+++ firebase-fun/index.ts
@@ -27,19 +27,25 @@ let app;
 
 const db = app.firestore();
 
-/*messaging().getToken().then((currentToken) => {
-	messagingToken=currentToken;
-});*/
-//go to db and get orders where status is 2 (pending)
+/*TOOPTIMIZE REALLY SH**** CODE AHEAD, DONT MERGE CONTEXTS not worth*/
 export const getCanceledOrders = async () => {
 	//orders where status is 5 and the order id is not present on notifications causeOrder
-	const orders = await db.collection("orders").where("status", "==", 5).get();
+	const orders = await db.collection("orders").where("status", "==", 5).where("addressed", "==", false).get();
+    
 	
-  return orders.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+	
+	return orders.docs.map((doc) => ({ id: doc.id, ...doc.data() })) ;
+}
+export const getPickupOrders = async () => {
+	//orders where status is 2 and the order id is not present on notifications causeOrder
+	const orders = await db.collection("orders").where("status", "==", 2).where("addressed", "==", false).get();
+	
+	return orders.docs.map((doc) => ({ id: doc.id, ...doc.data() })) ;
 }
 
 getCanceledOrders().then((orders) => {
-  console.log("Notifying canceled orders!: "+orders);
+  console.log("Notifying canceled orders!: ");
+  printArray(orders);
   //foreach canceled order create a notification where causeOrder is the order id, created_on is the current time, message is "A tua encomenda foi cancelada porque: "+ cancel_justification  and owner is the assigned_driver
   orders.forEach((order) => {
 	db.collection("notifications").add({
@@ -48,11 +54,17 @@ getCanceledOrders().then((orders) => {
 	  message: "A tua encomenda foi cancelada porque: " + order.cancel_justification,
 	  owner: order.assigned_driver,
 	});
-	
+	addressOrder(order).catch((error) => {
+		console.log("Error updating document: ", error);
+	});
 	//send notification with topic order.assigned_driver
 	console.log("Sending notification to: "+order.assigned_driver)
 	const message = {
-		
+		notification: {
+			title: "Encomenda "+order.id.slice(0,6)+" cancelada",
+			body: "A tua encomenda foi cancelada porque: " + order.cancel_justification,
+			
+		}, 
 		android: {
 		  priority: "HIGH",
 		  notification: {
@@ -79,3 +91,56 @@ getCanceledOrders().then((orders) => {
  
 
 });
+getPickupOrders().then((orders) => {
+	console.log("Notifying pickup orders!: ");
+	printArray(orders);
+	orders.forEach((order) => {
+		db.collection("notifications").add({
+		  causeOrder: order.id,
+		  created_on: new Date(),
+		  message: "A encomenda "+order.id.slice(0,6)+" está pronta para ser recolhida",
+		  owner: order.assigned_driver,
+		});
+		addressOrder(order).catch((error) => {
+			console.log("Error updating document: ", error);
+		});
+		const message = {
+			notification: {
+				title: "Pickup! Order: "+order.id.slice(0,6),
+				body: "A encomenda "+order.id.slice(0,6)+" está pronta para ser recolhida",
+				
+			}, 
+			android: {
+			  priority: "HIGH",
+			  notification: {
+				title: "Pickup! Order: "+order.id.slice(0,6),
+				body: "A encomenda "+order.id.slice(0,6)+" está pronta para ser recolhida",
+			  },
+			},
+			topic: order.assigned_driver,
+		  };
+		fcm.send(message, function (err, response) {
+		if (err) {
+			console.log("Something has gone wrong!");
+			console.log(err);
+		} else {
+			console.log("Successfully sent with response: ", response);
+		}
+	}); 
+		
+	});
+
+}); 	
+const addressOrder= async (order) => {
+	//set order.addressed to true
+	return db.collection("orders").doc(order.id).update({
+		addressed: true,
+	});
+
+
+}
+const printArray= (array) => {
+	array.forEach((element) => {
+		console.log(element);
+	});
+}
